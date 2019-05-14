a30c6c4546ba6fdfed9a6cd0f42cd44f071c7d42
Michael Harris <mhguitar1997@gmail.com>
2019-05-14 14:12:57 -0700

    More event listeners added

--- js/index.js
+++ js/index.js
@@ -2,12 +2,18 @@
 const funBus = document.querySelector('.logo-heading');
 const nav = document.querySelector('.nav');
 const navBorder = document.querySelector('.main-navigation');
-const navLinks = document.querySelectorAll('nav-links');
+const navLinks = document.querySelectorAll('.nav-link');
 const busImage = document.querySelector('.home .intro img');
+const buttons = document.querySelectorAll('.btn');
+const h2 = document.querySelectorAll('h2');
+const allImages = document.querySelectorAll('img');
 let lastScrollTop = 0;
+let startWidth = 0;
 
 funBus.addEventListener('mousemove', () => funBus.style.transform = 'scale(1.2) rotate(-8deg)');
+
 document.addEventListener('keydown', (event) => event.key === 'ArrowLeft' ? nav.style.transform = "translateX(-20px)" : nav.style.transform = "translateX(20px)")
+
 //Scroll
 document.addEventListener('scroll', () => {
     let st = window.pageYOffset || document.documentElement.scrollTop;
@@ -18,12 +24,44 @@ document.addEventListener('scroll', () => {
        navBorder.style.borderColor = 'white';
     }
 })
-//Drag
-navLinks.forEach(element => element.addEventListener('drag', (event) => {
-    console.log(event);
+
+//
+navLinks.forEach(element => element.addEventListener('click', (event) => {
+    event.preventDefault();
 }));
+
 //Load
 window.addEventListener('load', () => {
-    console.log('tyest');
     busImage.style.transform = 'translateY(0)';
+});
+
+//Focus
+buttons.forEach(element => element.addEventListener('focus', (event) => {
+    console.log(event)
+}))
+
+//resize
+window.addEventListener('resize', (event) => {
+    const newWidth = event.target.innerWidth;
+    if(newWidth > startWidth){
+        funBus.textContent = 'Fun Bus is now bigger!!'
+        startWidth = newWidth;
+    } else {
+        funBus.textContent = 'Fun Bus is now smaller'
+        startWidth = newWidth;
+    }
 })
+
+//Double click
+h2.forEach(element => element.addEventListener('dblclick', () => {
+    element.style.color = 'red';
+    element.style.fontWeight = '700';
+}))
+
+//Drag 
+allImages.forEach(img => img.addEventListener('drag', () => {
+    img.style.border = '5px solid black';
+}))
+allImages.forEach(img => img.addEventListener('dragend', () => {
+    img.style.border = 'none';
+}))
