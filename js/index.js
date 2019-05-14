e975aaba33f3e86a2c6fa46bc149b6be20755b74
Michael Harris <mhguitar1997@gmail.com>
2019-05-14 13:09:31 -0700

    First three event listeners added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const funBus = document.querySelector('.logo-heading');
+const nav = document.querySelector('.nav');
+const navBorder = document.querySelector('.main-navigation');
+let lastScrollTop = 0;
+
+funBus.addEventListener('mousemove', () => funBus.style.transform = 'scale(1.2) rotate(-8deg)');
+document.addEventListener('keydown', (event) => event.key === 'ArrowLeft' ? nav.style.transform = "translateX(-20px)" : nav.style.transform = "translateX(20px)")
+//Scroll
+document.addEventListener('scroll', () => {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    console.log(st);
+    if (st > lastScrollTop){
+       navBorder.style.borderColor = 'black';
+    } else {
+       navBorder.style.borderColor = 'white';
+    }
+})
